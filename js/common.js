8f7fb8d8c49ced61fd90430f483fac5e86fa9c3a
Kirill Bogatikov <kirillbogatikoff@gmail.com>
2020-05-20 21:10:47 +0300

    new epoch

--- js/common.js
+++ js/common.js
@@ -1,226 +1,62 @@
-/**
- * Callback listeners array
- */
-var onresizes = [];
-
-/**
- * Current language. Used in I18n
- */
-LANGUAGE = "ru";
-
-SUPPORTED_ERRORS = [404];
-
-/**
- * Adds to quene callback to listen onresize event
- */
-function registerOnResize(fn) {
-    if(onresizes.indexOf(fn) == -1)
-        onresizes.push(fn);
-    else
-        throw "Already registered";
+onload = function() {
+	console.log(screen)
+	if (screen.width % 1920 == 0 && screen.height % 1080 == 0) {
+		prepare_pagination()
+	} else {
+		$("#loading").hide();
+		$("#warning").show();
+	}	
 }
 
-/**
- * Add new onload listener
- */
-$(window).on("load", function() {
-    //start listening to onresize events
-    startOnResize();
-    
-    //add onscroll listen to animate fading "top" anchor
-    $(document).on("scroll", toggleFastScroll);
-    
-    $(document.body).on("click", "A", function(e) {
-	e.preventDefault();
-	openPage(e.currentTarget.href);
-    });
-    //add animation to all elements with class anchor-link
-    setScrollToAnimation(".anchor-link");
-});
+function accept_warning() {
+	console.log("FUCKUP MODE ACTIVATED")
+	prepare_pagination()
+}
 
-var lastScrollTop = -1;
-var fastScrollShowed = true;
+function prepare_pagination() {
+	$("#block").hide({ complete: function(){ $("#block").remove() } })
+	
+	var pagination = $("#pagination")
 
-/**
- * fades in or fades out top anchor 
- */
-function toggleFastScroll() {
-    var scroll = $(document.scrollingElement);
-    var scrollTop = scroll.prop("scrollTop");
+    var preventIndex = sessionStorage.getItem("preventIndex")
+    var onepage_scroll_params = {
+        sectionContainer: "section",
+        animationTime: 800,
+        pagination: false,  
+        keyboard: true,
+        direction: "vertical",
+        beforeMove: function(index) {
+            index--;
             
-    if(scrollTop > scroll.height() / 7 && !fastScrollShowed) {
-        fastScrollShowed = true;
-        $("#fast-scroll-top").animate({
-            opacity: 1
-        }, 300);
-    } 
-    if(scrollTop <= scroll.height() / 7 && fastScrollShowed) {
-        fastScrollShowed = false;
-        $("#fast-scroll-top").animate({
-            opacity: 0
-        }, 300);
-    }
-}
-
-//last screen params
-var lastScreen = {
-    width: -1,
-    height: -1
-};
-
-/**
- * listens screen resizing and calls all existing callbacks 
- */
-function startOnResize() {
-    if(lastScreen.width != screen.width || lastScreen.height != screen.height) {
-        lastScreen.width = screen.width;
-        lastScreen.height = screen.height;
-        
-        fixHeaderWidth();
-        fixHeaderNavigation();
-        fixSectionsPosition();
-        
-        if(onresizes.length > 0) {
-            for(var id in onresizes)
-                onresizes[id]();
-        }
-    }
+            if (index == preventIndex) return
     
-    //listen after 40 milliseconds
-    setTimeout(startOnResize, 40);
-}
+            if(index > 0) {
+                $("#pagination #section-0").css("opacity", 1)
+                $("#pagination #section-" + index).css("opacity", 1)
+            } else {
+                $("#pagination #section-0").css("opacity", "")
+            }
 
-/**
- * fixes header width: recount width by 
- * removing from width paddings 
- */
-function fixHeaderWidth() {
-    var header = $("#header");
-    header.width("100%");
-    var width = parseInt(header.width());
-    width -= (parseFloat(header.css("padding-left")) + parseFloat(header.css("padding-right")));
-    header.width(width);
-}
+            if (preventIndex > 0) {
+                $("#pagination #section-" + preventIndex).css("opacity", "")
+            }
 
-function fixHeaderNavigation() {
-    var header = $("#header");
-    var leftBlock = header.children(".left-text");
-    var rightBlock = $("#navigation");
-    
-    links = rightBlock.children(".anchor-link");
-    
-    var maxwidth = header.width() - leftBlock.width() * 1.33;
-    
-    if(rightBlock.width() < maxwidth) {
-	var i = 0;
-	var f = function() {
-	    if(rightBlock.width() < maxwidth && i < links.length) {
-    	    	$(links[i]).fadeIn(0, f);
-    	    	i++;
-	    }
-	};
-	f();
-    } else {
-	var i = 0;
-	var f = function() {
-	    if(rightBlock.width() > maxwidth && i < links.length) {
-		$(links[i]).fadeOut(0, f);
-		i++;
-	    }
-	};
-	f();
+            preventIndex = index
+            sessionStorage.setItem("preventIndex", index)
+        }    
     }
-};
+    $("main").onepage_scroll(onepage_scroll_params)
 
-
-/**
- * fixes sections position: shift all to header height pixels to bottom
- * header will be drawn on top of page. this will cause the content to overlap
- */
-function fixSectionsPosition() {
-    var preview = $($(".section")[0]);
-    var header = $("#header");
-    preview.css("padding-top", header.height() + 45);
-}
-
-/**
- * returns instance of XMLHttpRequest, associated with specified url
- * or array [errorStatus, errorMessage] if error was be occurred
- */
-function fetchFile(url, method) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(method ? method : 'GET', url, false);
-    xhr.send(null);
-
-    if (xhr.status != 200) {
-        return new Array(xhr.status, xhr.statusText);
+    if (preventIndex != undefined) {
+        preventIndex = parseInt(preventIndex) + 1
+        $("main").moveTo(preventIndex)
     }
-    return xhr;
-}
 
-/**
- * returns JSON structure, parsed from specified by url file's text
- * or array [errorStatus, errorMessage] if error was be occurred
- */
-function fetchJson(url) {
-    var file = fetchFile(url);
-    if(file instanceof Array) {
-        return file;
+    pagination.css("top", (document.body.clientHeight - pagination.height()) / 2)
+    for(var i in pagination.children()) {
+        eval(`function onclick() {
+            $("main").moveTo(` + i + ` + 1);
+        }`)
+        pagination.children()[i].onclick = onclick;
     }
-    return JSON.parse(file.responseText);
-}
-
-/**
- * opens specified link in current tab and adds record to history
- */
-function openPage(uri) {
-    if(uri.match("(ht|f)tp(s?):")) {
-	location.assign(uri);
-    } else {
-	$.ajax({
-	    url: uri,
-	    method: "HEAD"
-	}).done(function(data) {
-	    location.assign(uri);
-	}).fail(function(xhr) {
-	    var page = "unknown.html?" + xhr.status;
-	    if(~SUPPORTED_ERRORS.indexOf(xhr.status)) {
-		page = xhr.status + ".html";
-	    }
-	    location.assign("/error/" + page);
-	});
-    }
-}
-
-/**
- * sets scrolling animation to all elements, which found by selector
- * element must have href attribute with anchor name in value 
- */
-function setScrollToAnimation(selector) {
-    var anchors = $(selector);
-    anchors.on("click", function(event) { 
-        var id  = $(this).attr('href');
-        if(!id || id[0] != "#") {
-            return;    
-        } 
-        event.preventDefault();
-
-        animateScrollingTo($(id));
-    });
-}
-
-/**
- * scrolls page to specified element
- * child must be jquery instance
- */
-function animateScrollingTo(child) {
-    var minus = parseInt($("#header").height());
-    minus += parseInt(child.css("padding-top"));
-    minus += parseInt(child.css("margin-top"));
-    
-    var top = child.offset().top - minus;
-    
-    $(document.scrollingElement).animate({
-        scrollTop: Math.max(top, 0)
-    }, 500);
 }
